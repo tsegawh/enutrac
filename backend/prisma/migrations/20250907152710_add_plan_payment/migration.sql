0c0ede850b10ab7208a7b6c31922e6f3063186af
tsegawh <35263734+tsegawh@users.noreply.github.com>
2026-01-07 18:38:48 +0300

    Deleted migration_lock.toml

--- backend/prisma/migrations/20250907152710_add_plan_payment/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `payments` DROP FOREIGN KEY `payments_userId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `payments` ADD CONSTRAINT `payments_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/prisma/migrations/20251109143742_add_traccar_userid_to_user/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `payments` ALTER COLUMN `updatedAt` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `users` ADD COLUMN `traccarUserId` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `payments` ADD CONSTRAINT `payments_planId_fkey` FOREIGN KEY (`planId`) REFERENCES `subscription_plans`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
