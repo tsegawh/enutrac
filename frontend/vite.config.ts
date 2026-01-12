66d58d000b60c9afce3998b09911db337010f24d
tsegawh <35263734+tsegawh@users.noreply.github.com>
2026-01-12 19:36:53 +0300

    Updated vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     ],
     proxy: {
       '/api': {
-        target: "https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--365214aa.local-credentialless.webcontainer-api.io/", 
+        target: "http://localhost:3001/", 
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
