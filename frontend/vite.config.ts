a43201d6fefbe4dd4236124be6d1daa7ab38df57
tsegawh <35263734+tsegawh@users.noreply.github.com>
2026-01-13 16:33:03 +0300

    Updated vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     ],
     proxy: {
       '/api': {
-        target: "http://localhost:3001/", 
+        target: "http://localhost:3001", 
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
