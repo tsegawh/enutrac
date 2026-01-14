9389f466f24fc3bb655678a8c0bb5151c85897f3
tsegawh <35263734+tsegawh@users.noreply.github.com>
2026-01-14 18:49:12 +0300

    Updated prisma.config.ts

---
+++ prisma.config.ts
@@ -0,0 +1,10 @@
+// prisma.config.ts
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL!,
+    },
+  },
+});
