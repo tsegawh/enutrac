a92648ea46e27cdc33b4e4743e57223567ca0746
tsegawh <35263734+tsegawh@users.noreply.github.com>
2026-01-20 16:36:13 +0300

    Updated prisma-test.mjs

--- backend/prisma-test.mjs
+++ backend/prisma-test.mjs
@@ -1,4 +1,4 @@
-cat > prisma-test.mjs << 'EOF'
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -6,4 +6,4 @@ const prisma = new PrismaClient();
 await prisma.$connect();
 console.log("Prisma OK");
 process.exit(0);
-EOF
+
