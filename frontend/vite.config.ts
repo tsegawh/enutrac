c7b2d24a2554a3a92baf74527a7f476849bec7af
tsegawh <35263734+tsegawh@users.noreply.github.com>
2026-01-20 17:47:39 +0300

    Updated vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     allowedHosts: [
       'localhost',
       '127.0.0.1',
-     '192.168.0.101',
+     'https://1ffa66361689.ngrok-free.app',
      // optional: allow network host
     ],
     proxy: {
